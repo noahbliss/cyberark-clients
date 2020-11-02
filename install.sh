b2251e98202bd5a61082c364258ff683946e7782
Noah <noah@superuser.sh>
2020-11-02 17:10:14 -0500

    Basic functionality in install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,8 @@
 
 
 mkdir -p /usr/local/bin /usr/local/share/applications 
+if [ -d desktop ]; then cp -r desktop/* /usr/local/share/applications/; fi
+if [ -d bin ]; then cp -r bin/* /usr/local/bin/; fi
 
+echo "Installed, you may need to log out and back in for CA-RDP to show in your desktop environment's application list."
 
